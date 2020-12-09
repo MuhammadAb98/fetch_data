9dff11f7e039c656c2ca5a8d5fb9546cd32afe63
Muhammad Abdullah <56094500+MuhammadAb98@users.noreply.github.com>
2020-12-10 00:35:33 +0500

    Doing it

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import './App.css';
 
 function App() {
-  
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then((response) => response.json())
-  .then((json) => console.log(json))
-  },[])
-  
+
+ // const [data,setData]=useState({});
+
+useEffect(async()=>{
+
+const response= await fetch("https://api.github.com/users/muhammadab98/repos")
+const data=await response.json();
+ console.log(data);
+ 
+//    fetch('https://api.github.com/users/muhammadab98/repos')
+//  .then((response) => response.json())
+//  .then(json =>{
+//    console.log(json);
+//   setData(json);
+//    })
+
+ },[])
+
   return (
     <div>
-     <h1>Hello</h1>
+     <h1>Hello </h1>
     </div>
   );
 }
+
+
+
 
 export default App;
